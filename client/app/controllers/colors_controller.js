2c589a221c0e4696deecee902e768514d5212244
Rebecca C <20203tn077@utez.edu.mx>
2023-05-04 22:25:02 -0600

    Correción menor hecha en examen pero no subida en el momento

--- client/app/controllers/colors_controller.js
+++ client/app/controllers/colors_controller.js
@@ -1,12 +1,17 @@
 const conColors = document.getElementById('conColors')
 const template = `<div class="border d-inline-block" style="height: 30px; width: 30px; background-color: $color;"></div>`
 
-setInterval(() => api.fetch('colors').then(res => res.json()).then(colors => {
-    const content = []
-    for (const row of colors) {
-        let rowContent = ''
-        for (const color of row) rowContent += template.replace('$color', color)
-        content.push(rowContent)
-    }
-    conColors.innerHTML = content.join('<br>')
-}), 1000)
+function getColors() {
+    api.fetch('colors').then(res => res.json()).then(colors => {
+        const content = []
+        for (const row of colors) {
+            let rowContent = ''
+            for (const color of row) rowContent += template.replace('$color', color)
+            content.push(rowContent)
+        }
+        conColors.innerHTML = content.join('<br>')
+    })
+}
+
+getColors()
+setInterval(getColors, 1000)
